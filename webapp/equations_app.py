7cc7d86284adf944157fa0c219e38d08d66bdd93
smingolelli <49572529+smingolelli@users.noreply.github.com>
2019-09-28 14:59:44 -0400

    Update equations_app.py

--- webapp/equations_app.py
+++ webapp/equations_app.py
@@ -1,6 +1,17 @@
-from flask import Flask
-app = Flask(__name__)
+#============================================================================
+# Name        : equations_web_server.py
+# Author      : Stephen Mingolelli, Tinkernut
+# Description :  Web server for the equations game using Flask.
+#============================================================================
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+from flask import Flask
+
+server = Flask(__name__)
+
+@server.route("/")
+
+def Main():
+    return "Equations! The Game!"
+
+if __name__ == "__main__":
+    server.run(debug = True, host = "0.0.0.0", port = 80)
