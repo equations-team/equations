52633bd0d171ca5637641fe8adad3e454023262c
smingolelli <smingolelli@stetson.edu>
2019-09-29 16:23:18 -0400

    This is the webface. It's just a template, so it does not do anything, and it does not connect yet.

--- webapp/equations_flask_app.py
+++ webapp/equations_flask_app.py
@@ -4,14 +4,14 @@
 # Description :  Web server for the equations game using Flask.
 #============================================================================
 
-from flask import Flask
+from flask import Flask, render_template
 
 server = Flask(__name__)
 
 @server.route("/")
 
 def Main():
-    return "Equations! The Game!"
+    return render_template('index.html')
 
 if __name__ == "__main__":
     server.run(debug = True, host = "0.0.0.0", port = 80)
