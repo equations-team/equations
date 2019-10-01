95d6409ee10b4c85fa35888126ac5e170b2a9070
smingolelli <smingolelli@stetson.edu>
2019-10-01 19:46:52 -0400

    Updated comments.
    The comments are more clear and the description gives a more accurate summary of what this program does and uses.

--- webapp/equations_flask_app.py
+++ webapp/equations_flask_app.py
@@ -1,7 +1,9 @@
 #============================================================================
 # Name        : equations_web_server.py
 # Author      : Stephen Mingolelli, Tinkernut
-# Description :  Web server for the equations game using Flask.
+# Description :  Web server for the equations game using Flask. It refers
+# to a 'static' and 'templates' directory, which act as the source material
+# for any scripts, images, text, etc. that will appear on a page.
 #============================================================================
 
 from flask import Flask, render_template
@@ -10,8 +12,8 @@ server = Flask(__name__)
 
 @server.route("/")
 
-def Main():
-    return render_template('index.html')
+def main():
+    return render_template('index.html') #Index refers to the 'main page'
 
 if __name__ == "__main__":
     server.run(debug = True, host = "0.0.0.0", port = 80)
