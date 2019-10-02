ac18237f2ab0b3f7cbc6fbcaf7bf3974e4706bec
Rubyw123 <ywang4@stetson.edu>
2019-10-01 23:22:01 -0400

    push test

--- state_manager/Dice.java
+++ state_manager/Dice.java
@@ -1,18 +1,18 @@
 package test;
 import java.util.Random;
 
-public class Dice
+public class Dice 
 {
     private Random r;
     private String color;
     private String[] values;
-    
+     
     public Dice(String[] s, String clr) {
         r = new Random();
         values = s;
         color = clr;
     }
-    
+     
     public String roll() {
         int temp = r.nextInt(6);
         return values[temp];
