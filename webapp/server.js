e4166d315e40cf3d67a6a94b59718d1db64f5643
smingolelli <smingolelli@stetson.edu>
2019-10-06 17:30:48 -0400

    Turns, adding players, and moving
    This code adds a logic for how players will join and play the game.

--- webapp/server.js
+++ webapp/server.js
@@ -1,13 +1,74 @@
-/***********************************
+/**************************************************
 * Server Code
-* Author, Stephen Mingolelli, Mia
-************************************/
+* Author, Stephen Mingolelli, Mia, Ethan Zimmerman
+***************************************************/
 let players = [];
 let current_turn = 0;
 let timeOut;
 let _turn = 0;
 const MAX_WAITING = 5000;
 
+var validgame = function(req){
+  // What must exist in order for the game to start
+  if(!req.session.username) {return null; }
+  if(!req.session.gameID) {return null; }
+
+  return{
+    gameID : req.session.gameID;
+    name : req.session.username;
+  };
+};
+
+var IO = null;
+
+// When a player joins
+
+var join = function(gameID){
+  var sess = this.handshake.session;
+  var debugInfo = {
+    socketID : this.id,
+    event : 'join',
+    gameID : gameID,
+    session: sess
+  };
+
+  // Add this player to the actual game
+  var result = game.addPlayer(sess);
+  if(!result){
+    console.log('ERROR: Failed to Add Player', debugInfo);
+    this.emit('error', {message: "Not able to join this game."});
+    return;
+  }
+
+  // When joined they should be added to a room matching that game
+  this.join(gameID);
+
+  // Update everyone on the connection
+
+  IO.sockets.in(gameID).emit('update', game);
+};
+
+// When a player makes a move
+
+var move = function(data){
+  var sess = this.handshake.session;
+  var debugInfo = {
+    socketID : this.id,
+    event : 'move',
+    move : data.move,
+    session: sess
+  };
+  //var result = game.move(data.move)
+  if(!result){
+    console.log('Invalid move!', debugInfo)
+    this.emit('error', {message: "Invalid move, try again."});
+    return;
+  }
+
+  // Update this move to everyone else
+  IO.sockets.in(data.gameID).emit('update', game);
+  console.log(data.gameID+' '+sess.playerName+': '+data.move);
+};
 
 function next_turn(){
   _turn = current_turn++ % players.length;
