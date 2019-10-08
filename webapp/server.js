2248b6a1a01ed6ab8c367690d6a028672716b496
smingolelli <smingolelli@stetson.edu>
2019-10-08 15:49:14 -0400

    Updated server.js
    Made a few changes and added turn changes when a player leaves or makes their move.

--- webapp/server.js
+++ webapp/server.js
@@ -60,17 +60,96 @@ var move = function(data){
   };
   //var result = game.move(data.move)
   if(!result){
-    console.log('Invalid move!', debugInfo)
+    console.log('Invalid move!', debugInfo);
     this.emit('error', {message: "Invalid move, try again."});
     return;
   }
 
   // Update this move to everyone else
   IO.sockets.in(data.gameID).emit('update', game);
-  console.log(data.gameID+' '+sess.playerName+': '+data.move);
+  console.log(data.gameID+' '+sess.username+': '+data.move);
+
+  // Skip a turn, unless we want the game to end.
+  this.nextTurn;
 };
 
-function next_turn(){
+
+// When a player quits/forfeits the game.
+
+var withdraw = function(gameID){
+
+  var sess = this.handshake.session;
+  var debugInfo = {
+    socketID: this.id,
+    event :'withdraw',
+    gameID : gameID,
+    session: sess
+  };
+
+  // Begin withdrawal process
+  var result = game.withdraw(sess);
+  // Something goes wrong
+  if(!result){
+    console.log('Could not withdraw', debugInfo);
+    this.emit('Error', {message: "Could not withdraw from the game."});
+    return;
+  }
+
+  // Update this to the game and players
+
+  IO.sockets.in(gameID).emit('update', game);
+  console.log(gameID+' '+sess.username+'': Withdrew');
+
+  // Skip a turn, unless we want the game to end.
+  this.nextTurn;
+
+};
+
+// Removing a player from the game when they disconnect.
+
+var remove = function(){
+  var sess = this.handshake.session;
+  var debugInfo = {
+    socketID : this.id,
+    event : 'disonnect',
+    session : sess
+  };
+
+  // Remove this player
+
+  var result = game.removePlayer(sess);
+  if(!result){
+    console.log(sess.username+' failed to leave'+sess.gameID);
+    return;
+  }
+
+  // Make update to players
+
+  console.log(sess.username+' disconnected' +sess.gameID);
+  console.log('Socket '+this.id+' disconnected');
+
+  // Skip a turn, unless we want the game to end.
+  this.nextTurn;
+}
+
+// Attach events / functions to socket.io
+
+exports.attach = function(io){
+  IO = io;
+
+  io.sockets.on('connection', function (socket){
+
+    // Event handling
+    socket.on('join',join);
+    socket.on('move', move);
+    socket.on('withdraw', withdraw);
+    socket.on('remove', remove);
+
+    console.log('Socket'+socket.id+' connected');
+  })
+}
+
+function nextTurn(){
   _turn = current_turn++ % players.length;
   players[_turn].emit('your_turn');
   console.log("next turn triggered " , _turn);
