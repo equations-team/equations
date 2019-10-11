829792257d37445a13edf8287537638acd0319d0
nmingolelli <49539890+nmingolelli@users.noreply.github.com>
2019-10-10 21:04:19 -0400

    All of the code for the solver
    Following my last commit.

---
+++ test/src/solver/Main.java
@@ -0,0 +1,22 @@
+package solver;
+
+import javax.script.ScriptException;
+import solver.Algebra;
+import fundementalgamemechanics.*;
+import turnsystems.GameTurns;
+
+public class Main {
+
+	public static void main(String[] args) throws ScriptException {
+		
+		Game g = new Game();
+		
+		Algebra a = new Algebra("5+6","10+1");
+		
+		Solver manage = new Solver(a);
+		
+		System.out.println("Your answer equals: " + manage.calculate(a.getEquation()));
+		System.out.println("The real answer equals: " + manage.calculate(a.getSolution()));		
+	}
+
+}
