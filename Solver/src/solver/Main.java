33d914e674cb280ba820e345eeb843b7d570cbba
nmingolelli <49539890+nmingolelli@users.noreply.github.com>
2019-10-10 20:59:50 -0400

    New Name
    Lots of stuff. New name, path, code, in a group of commits.

---
+++ Solver/src/solver/Main.java
@@ -0,0 +1,22 @@
+package solver;
+import javax.script.ScriptException;
+
+import solver.Algebra;
+
+public class Main {
+
+	public static void main(String[] args) throws ScriptException {
+
+		Algebra a = new Algebra("5.0/6.0","10.0/12.0");
+		
+		Solver manage = new Solver(a);
+		
+		System.out.println("Your answer equals: " + manage.calculate(a.getEquation()));
+		System.out.println("The real answer equals: " + manage.calculate(a.getSolution()));	
+		System.out.println("Your answer is: " + manage.checkAnswer(a.getEquation()));
+		
+		System.out.println("This equation is valid: " + manage.validEquation(a.getEquation()));
+		
+	}
+
+}
