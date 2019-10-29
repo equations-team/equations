ac6881422ef8e855fdbfd479a6eec34107fc77cb
Derrek Woodworth <derrek.woodworth@gmail.com>
2019-10-29 13:40:14 -0400

    Refactored most of Java base into one eclipse project

---
+++ Equations/src/test/java/Equations/GoalTests.java
@@ -0,0 +1,42 @@
+package Equations;
+
+import java.util.Vector;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+import fundementalgamemechanics.DiceFace;
+import fundementalgamemechanics.Die;
+import fundementalgamemechanics.Goal;
+import fundementalgamemechanics.Mat;
+import fundementalgamemechanics.RedDie;
+import fundementalgamemechanics.SpecialDie;
+
+class GoalTests {
+	
+	@Test
+	public void removeDieTest() {
+		// Arrange
+	    final Goal testGoal = new Goal();
+
+	    // Act
+	    testGoal.addToMyMat(new RedDie());
+	    boolean testFalse = testGoal.removeDieByIndex(0);
+
+	    // Assert
+		assertEquals(false,testFalse);
+	}
+
+	@Test
+	public void addPerenthisesTest() {
+		// Arrange
+	    final Goal testGoal = new Goal();
+
+	    // Act
+	    testGoal.addPerenthises();
+
+	    // Assert
+		assertEquals(2,testGoal.getMyMat().size());
+	}
+
+}
