dc78ad6e46725184466b0c7f0924131d995e32dd
smingolelli <49572529+smingolelli@users.noreply.github.com>
2019-11-05 19:54:35 -0500

    Smingolellihtml5 (#38)
    * Fixed Server Code
    
    There were a couple typos and placement issues that needed to be resolved.
    
    * Created cgi-bin and equations_flask_app.cgi
    
    This has the script that will be used for getting data from fields (Username, password, e-mail). It does not have other data storing methods at the moment.
    
    * Added a line that stopped server crash.
    
    This line will more or less deal with index OOB issues.
    
    * Added actions and report_choice
    
    The server will report choices made by a user, but this method has been commented out.
    
    * Simplify Server using Pusher
    
    In order to make things (hopefully) easier I decided to use a mix of Flask and Pusher to make the multiplayer process easier. I'm still looking into how it works.
    
    * Created server.js
    
    Has turn increment logic. Not much else can be done without other data.
    
    * Turns, adding players, and moving
    
    This code adds a logic for how players will join and play the game.
    
    * Updated server.js
    
    Made a few changes and added turn changes when a player leaves or makes their move.
    
    * Added database, check validity, and finding game by id
    
    This makes use of a database. It does not make use of the one that we currently have. Connectivity of that will have to come later.
    
    * Added valid data system
    
    This will now validate data before a player can create or join a game. One problem I see, but I don't have an answer to, is that I don't know how to check if a room is full or how many players are in it.
    
    * Fixed moving and withdrawing
    
    Fixed lines that called the game variable, as it was not initialised.
    
    * Updated server.js, added package.json, and node.js
    
    * Made Equations_Controller.java and tests
    
    This is the controller. It interacts calls with the model, but when relevant, the view.

---
+++ controller/Equations_Controller.java
@@ -0,0 +1,179 @@
+import java.util.Random;
+
+import fundementalgamemechanics.Game;
+
+/**
+ * Controller for equations. It may also be called the presenter or medium for
+ * the user to interact. If the user moves a cube, then it is up to the
+ * controller to update that move to the model and view.
+ * 
+ * @author Stephen Mingolelli
+ *
+ */
+public class Equations_Controller {
+
+	private MockView myView;
+	private Game myModel;
+	private int myTurn;
+	private boolean myIsWon;
+
+	/**
+	 * The Constructor
+	 */
+	public Equations_Controller() {
+		myView = new MockView(this);
+		myModel = new Game();
+		myTurn = this.determineFirst();
+		myIsWon = false;
+		myView.setGoalSetter(myTurn);
+	}
+
+	/////////////
+	///METHODS///
+	/////////////
+	
+	/**
+	 * Roll dice and get the highest one. Each player rolls a die, the highest goes
+	 * first.
+	 * 
+	 * @return high, the highest number.
+	 */
+	private int determineFirst() {
+		int p1 = 0;
+		int p2 = 0;
+		int p3 = 0;
+		Random random = new Random();
+		p1 = random.nextInt(10 - 1 + 1) + 1;
+		p2 = random.nextInt(10 - 1 + 1) + 1;
+		p3 = random.nextInt(10 - 1 + 1) + 1;
+		int high = Math.max(p1, Math.max(p2, p3));
+		myTurn = high;
+		if(high == p1) {
+			myTurn = 1;
+		}else if(high == p2){
+			myTurn = 2;
+		}else if(high == p3) {
+			myTurn = 3;
+		}
+		return myTurn;
+	}
+
+	/**
+	 * This takes in view knowledge of the mat. More specifically, what the player
+	 * is requesting to be put on the 'goal' portion of the mat. Update the model
+	 * with this information. Index is the position of the die in a list of die.
+	 * 
+	 * @param index
+	 */
+	public void goalSet(int index) {
+		// Send die to goal
+		myModel.moveDie(index, 0);
+		passTurn();
+
+	}
+
+	/**
+	 * Skip to the next persons turn. It should be 1 if the last turn is player 3's.
+	 */
+	public void passTurn() {
+		if (myTurn == 3) {
+			myTurn = 1;
+			myView.setTurn(1);
+		} else {
+			myTurn++;
+			myView.setTurn(myTurn);
+		}
+	}
+
+	/**
+	 * Moves a die to one of three locations, forbidden, required, or allowed.
+	 * 
+	 * @param location
+	 * @param index
+	 */
+	public boolean moveDie(int location, int index) {
+		switch (location) {
+		case 0:
+			return this.moveToForbidden(index);
+		case 1:
+			return this.moveToRequired(index);
+		case 2:
+			return this.moveToAllowed(index);
+
+		}
+
+		// If we're lucky, this shouldn't occur.
+		System.out.println("ERROR");
+		return false;
+
+	}
+
+	/**
+	 * Move die to forbidden, uses die from resources at the index position.
+	 * 
+	 * @param index
+	 * @return true, if successful.
+	 */
+	private boolean moveToForbidden(int index) {
+		myModel.moveDie(index, 1);
+		if(myModel.moveDie(index, 1) == -1) { return false; }
+		 
+		return true;
+
+	}
+
+	/**
+	 * Move die to required, uses die from resources at the index position.
+	 * 
+	 * @param index
+	 * @return true, if successful.
+	 */
+	private boolean moveToRequired(int index) {
+		 myModel.moveDie(index, 2);
+		 if(myModel.moveDie(index, 2) == -1) { return false; }
+		 
+		return true;
+	}
+
+	/**
+	 * Move die to allowed, uses die from resources at the index position.
+	 * 
+	 * @param index
+	 * @return true, if successful.
+	 */
+	private boolean moveToAllowed(int index) {
+		 myModel.moveDie(index, 3); 
+		 if(myModel.moveDie(index, 3) == -1) { return false; }
+		 
+		return true;
+	}
+	
+	public void challengeImpossible(Algebra solution) {
+		//myGame.Solve...()
+	}
+	
+	public void challengeNow() {
+		//myGame.Solve...()
+	}
+	
+	/////////////////////////
+	///GETTERS AND SETTERS///
+	/////////////////////////
+
+	public int getTurn() {
+		return myTurn;
+	}
+
+	public void setTurn(int t) {
+		this.myTurn = t;
+	}
+
+	public boolean isWon() {
+		return myIsWon;
+	}
+
+	public void setIsWon(boolean w) {
+		this.myIsWon = w;
+	}
+
+}
